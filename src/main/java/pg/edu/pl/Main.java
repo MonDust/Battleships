a48b31e74d07e8a1cfb3aaa27c85bfdcc67ce4d0
alsus <s193584@student.pg.edu.pl>
2025-05-05 23:50:20 +0200

    Gameplay gui implementation

--- src/main/java/pg/edu/pl/Main.java
+++ src/main/java/pg/edu/pl/Main.java
@@ -2,10 +2,12 @@ package pg.edu.pl;
 
 import pg.edu.pl.game_mechanics.Game;
 import pg.edu.pl.game_mechanics.interfaces.IGame;
+import pg.edu.pl.gui.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
+        //MainWindow window = new MainWindow();
         game.startGameCLI();
     }
 }
